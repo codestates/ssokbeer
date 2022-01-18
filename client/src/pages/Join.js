e83833c68b39ba760a25320edfe8946acd196160
codingbe <descuento@naver.com>
2022-01-18 16:19:30 +0900

    Add: 회원가입 컴포넌터 초기 구현

---
+++ client/src/pages/Join.js
@@ -0,0 +1,72 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+  width: 100%;
+  margin: auto;
+  padding: 10px;
+`;
+const Form = styled.form`
+  width: 100%;
+  text-align: center;
+  max-width: 320px;
+`;
+const FormColumn = styled.div`
+  width: 100%;
+  margin-bottom: 20px;
+`;
+const Input = styled.input`
+  all: unset;
+  width: 100%;
+  border-bottom: 1px solid #aaa2a2;
+`;
+const Label = styled.label`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 30px;
+  color: gray;
+  font-weight: bold;
+`;
+const Button = styled.button`
+  all: unset;
+  padding: 15px 25px;
+  background-color: #fbf0d2;
+  border-radius: 8px;
+  cursor: pointer;
+  font-weight: bold;
+  color: gray;
+  &:hover {
+    background-color: #ffdc77;
+  }
+  transition: 0.4s;
+`;
+
+const Join = () => (
+  <Container>
+    <Form>
+      <FormColumn>
+        <Label>이메일</Label>
+        <Input placeholder='이메일을 입력해주세요' />
+      </FormColumn>
+      <FormColumn>
+        <Label>비밀번호</Label>
+        <Input placeholder='8자이상 / 영문 / 숫자를 조합해주세요' />
+      </FormColumn>
+      <FormColumn>
+        <Label>비밀번호 확인</Label>
+        <Input placeholder='비밀번호를 한번 더 입력해 주세요' />
+      </FormColumn>
+      <FormColumn>
+        <Label>닉네임</Label>
+        <Input placeholder='2~10글자' />
+      </FormColumn>
+      <Button>가입하기</Button>
+    </Form>
+  </Container>
+);
+
+export default Join;
